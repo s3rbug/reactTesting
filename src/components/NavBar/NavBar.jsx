ce049b58030784f669f72b94fa27c576efe864d2
s3rbug <bugaychuk.sergiy@gmail.com>
2019-12-01 22:20:48 +0200

    Added NavLink

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import classes from './NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
         <nav className={classes.nav}>
 
             <div className={classes.item}>
-                <a alt="Profile" href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a alt="Messages" href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a alt="News" href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={classes.item}>
-                <a alt="Music" href="/music">Music</a>
+                <NavLink to="/music">Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a alt="Settings" href="/settings">Settings</a>
+                <NavLink to="/settings">Settings</NavLink>
             </div>
 
         </nav>
