06ee1e4c30410413df6f8bcae2c779eed6ac6507
s3rbug <bugaychuk.sergiy@gmail.com>
2020-01-05 15:10:51 +0200

    Updates

---
+++ precache-manifest.03a399d187d902ecc97d2c6fe6efb686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe186a4a95b52a4470321468acfc5fa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "727868316ac8ea23bd77",
+    "url": "/static/css/main.2498d43c.chunk.css"
+  },
+  {
+    "revision": "2dad4333436127c38057",
+    "url": "/static/js/2.1e6da090.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.1e6da090.chunk.js.LICENSE"
+  },
+  {
+    "revision": "727868316ac8ea23bd77",
+    "url": "/static/js/main.1ac2d4e3.chunk.js"
+  },
+  {
+    "revision": "618db372b0320d357ab7",
+    "url": "/static/js/runtime-main.074b5909.js"
+  },
+  {
+    "revision": "2aea4710af286d8de340ad7a4e1de4ff",
+    "url": "/static/media/spinner.2aea4710.svg"
+  }
+]);
